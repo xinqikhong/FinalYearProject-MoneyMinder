6c90d0f4f04b6ea1616d1119f2b73de447517c05
xinqikhong <xinqi0620@gmail.com>
2024-04-20 00:49:22 +0800

    Update splashscreen.dart

--- stix3923/mypfm/lib/view/splashscreen.dart
+++ stix3923/mypfm/lib/view/splashscreen.dart
@@ -10,9 +10,34 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Splash Page"),
+        child: Container(
+          constraints: const BoxConstraints.expand(),
+          color: Color.fromARGB(255, 255, 248, 199),
+          // Set the background color here
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('assets/images/pfm_logo1.png'),
+              ),
+              Text(
+                "MoneyMinder",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              CircularProgressIndicator(
+                color: Colors.orangeAccent,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
