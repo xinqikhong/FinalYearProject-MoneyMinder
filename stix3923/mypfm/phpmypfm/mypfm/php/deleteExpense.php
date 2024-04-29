9d057f68ff9b263b7ee16fdd245759c2d5542416
xinqikhong <xinqi0620@gmail.com>
2024-04-30 07:41:59 +0800

    Add delete expense / income backend

---
+++ stix3923/mypfm/phpmypfm/mypfm/php/deleteExpense.php
@@ -0,0 +1,25 @@
+<?php
+if (!isset($_POST)) {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
+}
+
+include_once("dbconnect.php");
+
+$expenseId = $_POST['expense_id'];
+$sqldelete = "DELETE FROM `tbl_expense` WHERE expense_id = '$expenseId'";
+if ($conn->query($sqldelete) === TRUE) {
+    $response = array('status' => 'success', 'data' => null);
+    sendJsonResponse($response);
+} else {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+}
+
+
+function sendJsonResponse($sentArray)
+{
+    header('Content-Type: application/json');
+    echo json_encode($sentArray);
+}
---
+++ stix3923/mypfm/phpmypfm/mypfm/php/deleteIncome.php
@@ -0,0 +1,25 @@
+<?php
+if (!isset($_POST)) {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
+}
+
+include_once("dbconnect.php");
+
+$incomeId = $_POST['income_id'];
+$sqldelete = "DELETE FROM `tbl_income` WHERE income_id = '$incomeId'";
+if ($conn->query($sqldelete) === TRUE) {
+    $response = array('status' => 'success', 'data' => null);
+    sendJsonResponse($response);
+} else {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+}
+
+
+function sendJsonResponse($sentArray)
+{
+    header('Content-Type: application/json');
+    echo json_encode($sentArray);
+}
